b048aabd6fe302f5ef072d688e03c071e9c924f2
Tracker Developer <dev@tracker.local>
2026-02-10 17:42:31 +0800

    chore: 添加 gunicorn 生产环境配置

---
+++ dev/gunicorn.conf.py
@@ -0,0 +1,35 @@
+"""Gunicorn configuration for Tracker production server"""
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:8080"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Process naming
+proc_name = "tracker"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# Logging
+errorlog = "/var/log/tracker_error.log"
+accesslog = "/var/log/tracker_access.log"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+
+# Process management
+graceful_timeout = 30
+max_requests = 1000
+max_requests_jitter = 50
